a9276040ecedfe27a6b9017be8a3b12c3f96cda6
shekhartyagi <61544121+shekhu-tech@users.noreply.github.com>
2026-02-13 16:29:02 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -3,22 +3,22 @@
     const params = new URLSearchParams(window.location.search);
     const allow = params.get("allow");
 
-    // App / PWA allow
+    // allow only app / pwa
     if (allow === "stiskilli") {
       return;
     }
 
-    // SEO bots allow
+    // seo bots allow
     const ua = navigator.userAgent.toLowerCase();
     if (/bot|googlebot|crawler|spider|robot|crawling/i.test(ua)) {
       return;
     }
 
-    // Redirect baaki sab
+    // baaki sab redirect
     window.location.replace(
       "https://play.google.com/store/apps/details?id=com.ai.skillaura"
     );
   } catch (e) {
-    console.log("redirect error", e);
+    console.log("redirect handled");
   }
 })();
